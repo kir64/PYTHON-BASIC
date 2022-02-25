9b384ad325d85ac9804ce7965b0eeb9faa6771e3
kir64 <59437394+kir64@users.noreply.github.com>
2022-02-25 14:46:15 +0400

    4check

--- practice/1_python_part_1/task1.py
+++ practice/1_python_part_1/task1.py
@@ -11,6 +11,7 @@ Examples:
     >>> delete_from_list([], 'b')
     []
 """
+
 def delete_from_list(lst, x):
     i, n = 0, len(lst)
     while x in lst:
@@ -23,4 +24,3 @@ print(delete_from_list([1, 2, 3, 4, 3], 3))  # [1, 2, 4]
 print(delete_from_list(['a', 'b', 'c', 'b', 'd'], 'b'))  # ['a', 'c', 'd']
 print(delete_from_list([1, 2, 3], 'b'))  # [1, 2, 3]
 print(delete_from_list([], 'b'))  # []
-
