88dd455924239492e66795141edcd94f3ea2f3e1
kir64 <59437394+kir64@users.noreply.github.com>
2022-03-08 18:06:44 +0400

    4check

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -13,3 +13,16 @@ Example:
 
     result.txt(content: "23, 78, 3")
 """
+
+import os
+
+folder_path = '/Users/klatanov/PYTHON-BASIC/practice/2_python_part_2/files'
+folder = sorted(os.listdir(folder_path))
+ans_lst = []
+for file in folder:
+    file_path = folder_path + '/' + file
+    with open(file_path, 'r') as rf:
+        value = rf.read()
+        ans_lst.append(value)
+with open('result.txt', 'w') as wf:
+    print(', '.join(ans_lst), file=wf)
