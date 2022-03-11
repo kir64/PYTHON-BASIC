9dc4d4a600c60ad376b69d75230bbf10096273c4
kir64 <59437394+kir64@users.noreply.github.com>
2022-03-11 18:22:42 +0400

    Update task_1.py

--- practice/4_python_part_3/task_1.py
+++ practice/4_python_part_3/task_1.py
@@ -10,11 +10,29 @@ If entered date is from future, return negative value for number of days
     >>> calculate_days('10-07-2021')
     WrongFormatException
 """
-from datetime import datetime
+from datetime import *
 
 
-def calculate_days(from_date: str) -> int:
-    ...
+def calculate_days(custom_date='2021-12-24'):
+    try:
+        custom_date = datetime.strptime(custom_date, '%Y-%m-%d').date()
+        cur_date = date.today()
+        return cur_date.toordinal() - custom_date.toordinal()
+    except ValueError:
+        return 'WrongFormatExpression'
+
+
+print(calculate_days('2021-10-07'))  # for this example today is 6 october 2021
+# -1
+print(calculate_days('2021-10-05'))
+# 1
+print(calculate_days('10-07-2021'))
+# WrongFormatException
+print(calculate_days())
+# 77
+print(calculate_days('2022-04-01'))
+# -21
+
 
 
 """
