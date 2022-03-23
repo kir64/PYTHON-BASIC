ebce0ff64f0a166c99585b016129bb3c719f1627
kir64 <59437394+kir64@users.noreply.github.com>
2022-03-23 13:21:41 +0400

    4check

--- practice/3_python_testing/test_read_write_2.py
+++ practice/3_python_testing/test_read_write_2.py
@@ -4,3 +4,41 @@ To write files during tests use temporary files:
 https://docs.python.org/3/library/tempfile.html
 https://docs.pytest.org/en/6.2.x/tmpdir.html
 """
+
+import os
+import random
+import string
+import tempfile
+
+
+def task_read_write_2(file1_name, file2_name):
+    def generate_words(n=20):
+
+        words = list()
+        for _ in range(n):
+            word = ''.join(random.choices(string.ascii_lowercase, k=random.randint(3, 10)))
+            words.append(word)
+
+        return words
+
+    lst = generate_words()
+    with open(file1_name, 'w', encoding='UTF-8') as f1:
+        print(*lst, sep='\n', file=f1)
+    with open(file2_name, 'w', encoding='CP1252') as f2:
+        print(*reversed(lst), sep=',', file=f2)
+
+
+def test_task_read_write_2():
+    tmp_file = tempfile.NamedTemporaryFile()
+    task_read_write_2(tmp_file.name, 'file2.txt')
+    
+    with open(tmp_file.name, 'r') as rf:
+        txt_to_check = rf.read()
+    with open('file2.txt', 'r') as rf:
+        res_txt = rf.read()
+    
+    txt_to_check = ','.join(txt_to_check.strip().split('\n')[::-1])
+    assert res_txt == txt_to_check
+
+
+test_task_read_write_2()
