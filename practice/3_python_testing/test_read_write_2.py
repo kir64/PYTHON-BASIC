9c513f195c19b73e8b672e8d19f7ecbcb4a502de
kir64 <59437394+kir64@users.noreply.github.com>
2022-03-23 13:46:09 +0400

    4check

--- practice/3_python_testing/test_read_write_2.py
+++ practice/3_python_testing/test_read_write_2.py
@@ -12,7 +12,6 @@ import tempfile
 
 def task_read_write_2(file1_name, file2_name):
     def generate_words(n=20):
-
         words = list()
         for _ in range(n):
             word = ''.join(random.choices(string.ascii_lowercase, k=random.randint(3, 10)))
@@ -30,14 +29,14 @@ def task_read_write_2(file1_name, file2_name):
 def test_task_read_write_2():
     tmp_file = tempfile.NamedTemporaryFile()
     task_read_write_2(tmp_file.name, 'file2.txt')
-    
+
     with open(tmp_file.name, 'r') as rf:
         txt_to_check = rf.read()
     with open('file2.txt', 'r') as rf:
         res_txt = rf.read()
-    
+
     txt_to_check = ','.join(txt_to_check.strip().split('\n')[::-1])
-    assert res_txt == txt_to_check
+    assert res_txt.strip() == txt_to_check
 
 
 test_task_read_write_2()
