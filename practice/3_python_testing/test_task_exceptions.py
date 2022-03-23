619d53792f9466b96f27f04676533ca7330f3c1e
kir64 <59437394+kir64@users.noreply.github.com>
2022-03-23 14:18:50 +0400

    4check

--- practice/3_python_testing/test_task_exceptions.py
+++ practice/3_python_testing/test_task_exceptions.py
@@ -9,13 +9,40 @@ https://stackoverflow.com/a/20507769
 """
 
 
+class DivisionByOneException(ArithmeticError):
+    pass
+
+
+def division(x, y):
+    try:
+        if y == 1:
+            raise DivisionByOneException('Deletion on 1 get the same result')  
+            # "Division", not "Deletion" task should be fixed. 
+        return x // y
+    except ZeroDivisionError:
+        print('Division by 0')
+        return None
+    except DivisionByOneException as err:
+        print(f'DivisionByOneException("{err}")')
+        return None
+    finally:
+        print('Division finished')
+
+
 def test_division_ok(capfd):
-    ...
+    ans = division(32, 4)
+    out, err = capfd.readouterr()
+    assert out == 'Division finished\n'
+    assert ans == 8
 
 
 def test_division_by_zero(capfd):
-    ...
+    division(32, 0)
+    out, err = capfd.readouterr()
+    assert out == 'Division by 0\nDivision finished\n'
 
 
 def test_division_by_one(capfd):
-    ...
+    division(32, 1)
+    out, err = capfd.readouterr()
+    assert out == 'DivisionByOneException("Deletion on 1 get the same result")\nDivision finished\n'
