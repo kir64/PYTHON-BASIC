cb7b47343bbd61730407fe9028cacc7d5b1ce87e
kir64 <59437394+kir64@users.noreply.github.com>
2022-04-04 20:46:49 +0400

    4check

--- practice/4_python_part_3/task_2.py
+++ practice/4_python_part_3/task_2.py
@@ -13,9 +13,27 @@ import math
 
 
 def math_calculate(function: str, *args):
-    ...
-
+    if len(args) > 2:
+        raise AttributeError
+    try:
+        fun = getattr(math, function)
+    except AttributeError:
+        return 'OperationNotFoundException'
+    return fun(*args)
 
 """
 Write tests for math_calculate function
 """
+
+def test_math_calculate_1():
+    assert math_calculate('log', 1024, 2) == 10.0
+
+
+def test_math_calculate_2():
+    assert math_calculate('ceil', 10.7) == 11
+
+
+def test_math_calculate_3(capfd):
+    print(math_calculate('cel', 10, 7))
+    out, err = capfd.readouterr()
+    assert out == 'OperationNotFoundException\n'
